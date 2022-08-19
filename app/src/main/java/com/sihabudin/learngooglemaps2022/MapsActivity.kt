8c4ea684a8429916b21ab7d8b98f95111d9386e6
moha-sihab <seehabb@gmail.com>
2022-08-19 08:19:02 +0700

    Change map type and map style

--- app/src/main/java/com/sihabudin/learngooglemaps2022/MapsActivity.kt
+++ app/src/main/java/com/sihabudin/learngooglemaps2022/MapsActivity.kt
@@ -2,12 +2,16 @@ package com.sihabudin.learngooglemaps2022
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.android.gms.maps.model.MarkerOptions
 import com.sihabudin.learngooglemaps2022.databinding.ActivityMapsBinding
 
@@ -24,6 +28,56 @@ internal class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.map_type_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.normal_map -> {
+                mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+            }
+
+            R.id.hybrid_map -> {
+                mMap.mapType = GoogleMap.MAP_TYPE_HYBRID
+            }
+
+            R.id.satellite_map -> {
+                mMap.mapType = GoogleMap.MAP_TYPE_SATELLITE
+            }
+
+            R.id.terrain_map -> {
+                mMap.mapType = GoogleMap.MAP_TYPE_TERRAIN
+            }
+
+            R.id.none_map -> {
+                mMap.mapType = GoogleMap.MAP_TYPE_NONE
+            }
+
+
+        }
+
+        return true
+    }
+
+    private fun setMapStyle(googleMap: GoogleMap){
+        try{
+             val success = googleMap.setMapStyle(
+                MapStyleOptions.loadRawResourceStyle(
+                    this,
+                    R.raw.style
+                )
+             )
+
+            if(!success){
+                Log.d("Maps","Failed to add style.")
+            }
+        }
+        catch (e : Exception){
+            Log.d("Maps",e.toString())
+        }
+    }
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -42,5 +96,7 @@ internal class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             .position(sydney)
             .title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        setMapStyle(mMap)
     }
 }
